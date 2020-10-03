ec939680ab0cef5931537574b0e0e00142997784
daniellethrelfall <35313200+daniellethrelfall@users.noreply.github.com>
2020-10-03 14:39:14 +0100

    Add files via upload

---
+++ Treasure Hunt/js/rm3_task4.js
@@ -0,0 +1,24 @@
+// Code for room
+
+
+
+var answer = "6"; 
+
+function getAnswer() {
+  var x = document.getElementById("answer");
+
+  if(x.value != answer){
+        document.getElementById('answer').style.backgroundColor = "red"; //when answer is incorrcet turn red
+        return false;
+    }else{
+        document.getElementById('answer').style.backgroundColor = "green"; //when answer is correct turn green
+
+        var wellDone; //sets the variable for the text output
+        wellDone = "Well done! You've completed this task!";
+        document.getElementById("results").innerHTML = wellDone; //inserts 'text' into the yourscore ID eleme
+        
+        localStorage.setItem("CHANGE!!!!", "complete");
+    };
+}
+
+
